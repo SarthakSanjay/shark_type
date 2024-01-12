cc4dac4a8d4a8c3c2dfde138e29f5aa8d9340bd5
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-13 01:23:37 +0530

    testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -26,8 +26,9 @@ const App = () => {
         
       }
   },[])
-  return (
-    <div className='h-screen w-screen flex flex-col justify-center items-center bg-gray-800'>
+  return (<>
+    <div className='h-screen hidden lg:flex w-screen  flex-col justify-center items-center bg-gray-800'>
+    
     <Logo />
     <HighestWPM />
     <input
@@ -36,9 +37,11 @@ const App = () => {
         className='absolute opacity-0 pointer-events-none'
       />
         {display ? <DisplayArea  /> : <h1 className='text-3xl text-gray-400'>Press <span className='animate-pulse bg-gray-900 px-2 border rounded-lg'>Enter</span> to start</h1>}
-        <button className='absolute bottom-16 bg-purple-500 text-white mb-5 py-1 px-2 rounded-md border' onClick={handleButtonClick}>Open Keyboard</button>
+        <button className='absolute  lg:hidden bottom-16 bg-purple-500 text-white mb-5 py-1 px-2 rounded-md border' onClick={handleButtonClick}>Open Keyboard</button>
         <Footer />
     </div>
+    <div className='flex justify-center items-center bg-black text-white h-screen w-screen lg:hidden'>For mobiles work in progress please visit this website on pc</div>
+  </>
   )
 }
 
