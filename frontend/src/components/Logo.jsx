1de1c5b34ec234c4ea6951bf3b4c6e9a943e2c7c
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2024-01-12 21:04:42 +0530

    refactored code

---
+++ frontend/src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../App.css'
+const Logo = () => {
+  return (
+    <div id='logo' className='absolute top-0 left-0 p-10 text-[40px] text-blue-400'>SharkType</div>
+  )
+}
+
+export default Logo
